1847c567de2682683acd1a313cedc5462fc5d74f
Dynamoshots <divyam.businessinsights@gmail.com>
2026-04-22 05:46:13 +0530

    Update2 main.js

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import './style.css';
 const ANSWER      = 'DEBIT';
 const MAX_GUESSES = 6;
 const WORD_LENGTH = 5;
-const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzLZcO7vqeSh9Czwtr6GHMByYRiWyw_bRhZtqoRW_RMUey05epFH77GH8rvzWkvrXWc/exec';
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzHGwiyHKgArGclJvK39I0tmMF-xKQ2fpoUox46xROW3xgsCK5heYAzFfZA6PfNtuVv/exec';
 
 let playerEmail  = '';
 let currentRow   = 0;
